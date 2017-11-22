0a9ab9c95a1d611444455aa02ef6760d846c72c0
Evan Burke <git@capitalstudios.net>
2017-11-22 00:58:36 -0800

    having trouble passing cmd line args. working on it

--- light-schedule.py
+++ light-schedule.py
@@ -1,4 +1,4 @@
-import tradfri
+from tradfri import Tradfri
 import datetime
 import argparse
 
@@ -29,17 +29,49 @@ parser = argparse.ArgumentParser(conflict_handler='resolve')
 
 parser.add_argument("device", help="Friendly name of the device to update.")
 parser.add_argument("action", help="Action to take on the specified device. See tradfri.py for supported actions.")
-parser.add_argument("--params", "-p", default=None, help="Parameters for the specified action, as a dict.")
+parser.add_argument("params", nargs='*', default=None, help="Parameters for the specified action.")
 parser.add_argument("--verbose", "-v", default=0, type=int, choices=[1,2], help="Increase verbosity of output.")
 args = parser.parse_args()
 
 from pprint import pprint
-print("")
+print("args:")
 pprint(args)
+print("")
+print("params:")
+print(args.params)
+print("")
+
+#dvc = args['device']
+#print(dvc)
+
+device = Tradfri(devices[args.device]['entity_id'], debug=args.verbose)
+
+#pprint(device.get_attrs())
+
+func = getattr(device, args.action)
+
+if args.params:
+	if len(args.params) == 1:
+		resp = func(args.params)
+	else:
+		# need to do some parsing here. maybe subparsers? https://docs.python.org/3/library/argparse.html#sub-commands
+
+		resp = func(*args.params)
+else:
+	resp = func()
+
+if resp:
+	pprint(resp)
+
+#device.set_brightness(args.params)
+
+#device.toggle()
 
 
-data = tradfri.get_attrs(devices['geo_desk']['entity_id'])
-print(data['brightness'], data['color_temp'])
+
+
+#data = tradfri.get_attrs(devices['geo_desk']['entity_id'])
+#print(data['brightness'], data['color_temp'])
 
 
 
