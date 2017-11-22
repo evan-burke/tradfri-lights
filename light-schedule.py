7428f4c23ca8495a4e247d63747ec20e4332ff4c
Evan Burke <git@capitalstudios.net>
2017-11-21 17:46:09 -0800

    wip 2

--- light-schedule.py
+++ light-schedule.py
@@ -1,27 +1,28 @@
 import tradfri
 import datetime
+import argparse
 
-import json
-
-print("hello")
+#import json
 
 
-# light bulb id
-ent_id = "light.tradfri_bulb_e26_ws_opal_980lm"
+
+devices = { 
+	"office_table": 
+		{ "entity_id": "light.tradfri_bulb_e26_ws_opal_980lm" },
+	"geo_desk":
+		{ "entity_id": "light.tradfri_bulb_e12_ws_opal_400lm_2" }
+	}
+
+
+
+
 
 debug=1
 
-#tradfri.transition(ent_id, {"brightness": 192}, datetime.timedelta(minutes=1))
+tradfri.transition(devices['geo_desk']['entity_id'], {"brightness": 92}, datetime.timedelta(minutes=1))
+
 
 #transition(ent_id, {"color_temp": 2700}, datetime.timdelta(hours=2))
 
 #tradfri.set_brightness(ent_id, 192)
 
-data3 = tradfri.get_bulb_state(ent_id)
-print(data3)
-
-from pprint import pprint
-
-data = tradfri.apireq("states/" + ent_id, "get")
-state = json.loads(data.text)
-pprint(state)
